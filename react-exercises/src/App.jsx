b9888c71d25c7638e3a56a65d39b2be50e7337d1
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-15 19:43:33 +0100

    added folder components and subfolders and divided files css

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,32 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import Header from "./components/Header/Header.jsx";
 import {CORE_CONCEPTS} from "./data.js";
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} Fundamental React concepts you will need for almost any app you are going to build!
-            </p>
-        </header>
-    );
-}
-
-function CoreConcept(props) {
-    return <li>
-        <img src={props.image} alt={props.title}/>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-    </li>
-}
 
 function App() {
     return (
