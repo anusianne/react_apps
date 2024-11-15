ac051b9bda407047a7871637cee070bf4d5ae712
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-15 21:03:08 +0100

    added onclick events to buttons

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,8 +1,13 @@
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
+import TabButton from './components/TabButton.jsx'
 import {CORE_CONCEPTS} from "./data.js";
 
 function App() {
+
+    function handleClick(selectedButton) {
+        console.log(selectedButton)
+    }
     return (
         <div>
             <Header />
@@ -15,6 +20,15 @@ function App() {
                         <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
                         <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
                     </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton label="Components" onSelect={()=> handleClick('components')}></TabButton>
+                        <TabButton label="JSX" onSelect={()=> handleClick('jsx')}></TabButton>
+                        <TabButton label="Props" onSelect={()=> handleClick('props')}></TabButton>
+                        <TabButton label="State" onSelect={()=> handleClick('state')}></TabButton>
+                    </menu>
                 </section>
             </main>
         </div>
