d749ca583cce73c2d7b4a0408865521cf66ae737
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-15 18:51:56 +0100

    added new data and refactor id style

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import {CORE_CONCEPTS} from "./data.js";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -33,13 +33,13 @@ function App() {
         <div>
             <Header />
             <main>
-                <section id='core-concept'>
+                <section id='core-concepts'>
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept title="Components" description="The core UI building block." img={componentsImg}/>
-                        <CoreConcept title={} description="" img={}/>
-                        <CoreConcept title={} description="" img={}/>
-                        <CoreConcept title={} description="" img={}/>
+                        <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+                        <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+                        <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
                     </ul>
                 </section>
             </main>
