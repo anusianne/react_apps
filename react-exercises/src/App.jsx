417d7dbcd8e763019181e699cb407a7dd8f5b958
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-20 17:01:51 +0100

    added map array to conceptitems

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -20,10 +20,13 @@ function App() {
                 <section id='core-concepts'>
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-                        <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
-                        <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
-                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+                        {CORE_CONCEPTS.map((conceptItem) =>
+                          <CoreConcept {...conceptItem}/>
+                        )}
+                        {/*<CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>*/}
+                        {/*<CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>*/}
+                        {/*<CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>*/}
+                        {/*<CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>*/}
                     </ul>
                 </section>
                 <section id="examples">
