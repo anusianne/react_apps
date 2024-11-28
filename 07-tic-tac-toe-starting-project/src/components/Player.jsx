dcbedecbc944f41b6715884d6ddce559d270be9d
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-28 14:35:55 +0100

    added updateing button

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,18 +4,18 @@ export default function Player({name, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEdit() {
-         setIsEditing(true);
+         setIsEditing((editing) => !editing);
     }
 
     return (
         <li>
             <span className="player">
             {isEditing === false && <span className="player-name">{name}</span>}
-                {isEditing === true && <input type="text" required/>}
+                {isEditing === true && <input type="text" required velue={name}/>}
             <span className="player-symbol">{symbol}</span>
         </span>
 
-            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleEdit}>{isEditing? 'Save': 'Edit'}</button>
         </li>
     )
 }
