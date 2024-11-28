8956753a7ef1fa51686aae817c706019a78eefbd
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-11-28 21:36:18 +0100

    added handlechange input function

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
     const [isEditing, setIsEditing] = useState(false);
-
+    const [playerName, setPlayerName] = useState(initialName);
+    
     function handleEdit() {
          setIsEditing((editing) => !editing);
     }
+    function handleChange(e) {
+        setPlayerName(e.target.value)
+    }
+let editablePlayerName = <span className="player-name">{playerName}</span>
 
+if(isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+}
     return (
         <li>
             <span className="player">
-            {isEditing === false && <span className="player-name">{name}</span>}
-                {isEditing === true && <input type="text" required velue={name}/>}
+       {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-
             <button onClick={handleEdit}>{isEditing? 'Save': 'Edit'}</button>
         </li>
     )
