25c44b652d705fc59047fd591fb1a1917f72ab8b
ann_zielinska <100295263+anusianne@users.noreply.github.com>
2024-12-22 23:18:57 +0100

    Update Header.jsx

--- reactart/src/components/Header.jsx
+++ reactart/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt...</h1>
+      <h1>ReactArt..</h1>
       <p className={styles.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
