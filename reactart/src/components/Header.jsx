485caed0b15a8ac61213c73e2b74e375c0aaee73
ann_zielinska <100295263+anusianne@users.noreply.github.com>
2024-12-27 22:58:38 +0100

    Update Header.jsx

--- reactart/src/components/Header.jsx
+++ reactart/src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt="" />
+      <img src={logo} alt="reactart" />
       <h1>ReactArt..</h1>
       <p className={styles.paragraph}>A community of artists and art-lovers. Keep changes.</p>
     </header>
