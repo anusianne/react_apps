0df3e37ccdf4a5512c5cfa28171f69b35ffe0c0f
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-01-02 22:06:37 +0100

    new component with props and propTypes

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -4,6 +4,7 @@ import {useState} from 'react';
 // import Examples from "./components/Examples.jsx";
 // import CoreConcepts from "./components/CoreConcept/CoreConcepts.jsx";
 import Card from "./Card.jsx";
+import Student from "./Student.jsx";
 
 function App() {
 
@@ -18,6 +19,8 @@ function App() {
        <>
            <Card/>
            <Card/>
+           <Student name="Bob" age="30" isStudent={true}/>
+           <Student name="Patrick" age={42} isStudent={false}/>
        </>
     );
 }
