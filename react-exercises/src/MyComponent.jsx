4b3c9f855d1b6bfd678defd6144292aa48137ae8
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-01-10 15:57:42 +0100

    added updates objects in state

--- react-exercises/src/MyComponent.jsx
+++ react-exercises/src/MyComponent.jsx
@@ -1,23 +1,26 @@
 import {useState} from "react";
 
 export default function MyComponent() {
-    const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(22);
+    const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
 
-    const updateName = () => {
-        setName("SpongeBob");
+    function handleYearChange(event) {
+        setCar({...car, year: event.target.value})
     }
 
-    const incrementAge = () => {
-        setAge(age + 1)
+    function handleMakeChange(event) {
+        setCar({...car, make: event.target.value})
     }
+
+    function handleModelChange(event) {
+        setCar({...car, model: event.target.value})
+    }
+
     return (
         <div>
-            <p>Name: {name}</p>
-            <button onClick={updateName}>Set Name</button>
-
-            <p>Age: {age}</p>
-            <button onClick={incrementAge}>Increment Age</button>
+            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/>
         </div>
     )
 }
