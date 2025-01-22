a83e2204caf5d615b55b8673fb88c74752626a58
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-01-22 16:59:38 +0100

    added functions and basic input for adding task

--- react-exercises/src/components/ToDoList/ToDoList.jsx
+++ react-exercises/src/components/ToDoList/ToDoList.jsx
@@ -1,5 +1,37 @@
+import {useState} from "react";
+
 export default function ToDoList() {
-    return <>
+    const [tasks, setTasks] = useState('');
+    const [newTask, setNewTask] = useState('');
+
+    function handleTaskChange(e) {
+        setNewTask(e.target.value)
+    }
+
+    function addTask() {
+
+    }
+
+    function deleteTask(index) {
+
+    }
+
+    function moveTaskUp(index) {
+
+    }
+
+    function moveTaskDown(index) {
+
+    }
+
+    return <div className="toDoList">
         <h1>To Do List</h1>
-    </>
+        <div>
+            <input
+                type="text" value={newTask} placeholder="Enter a task:" onChange={handleTaskChange}
+            />
+            <button className="addButon" onClick={addTask}>Add Task</button>
+        </div>
+
+    </div>
 }
